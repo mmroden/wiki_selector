16a8cd5e5408485de0ed39e9a6a60beba1e2844b
Mark Roden <mmroden@gmail.com>
2016-06-18 17:35:55 -0700

    things aren't yet working

--- generate_collections.py
+++ generate_collections.py
@@ -20,18 +20,17 @@ from deap import creator
 from deap import tools
 
 ALL_FILES = prep_files()
+ALL_FILES_KEYS = list(ALL_FILES.keys())
+ALL_FILES_KEYSET = set(ALL_FILES_KEYS)
 PAGE_LINKS_INDEX = 3
 LANG_LINKS_INDEX = 4
 PAGE_VIEWS_INDEX = 5
 PAGE_SIZE_INDEX = 2
 
+IND_INIT_SIZE=10000
+
 creator.create("Fitness", base.Fitness, weights=(-1.0, 1.0, 1.0, 1.0))
 creator.create("Individual", Counter, fitness=creator.Fitness)
-
-toolbox = base.Toolbox()
-toolbox.register("attr_item", random.choice, ITEMS_NAME)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_item, IND_INIT_SIZE)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 
 def init_selection():
@@ -40,12 +39,20 @@ def init_selection():
     return the_keys[:random.randint(10, len(the_keys))]
 
 
+toolbox = base.Toolbox()
+toolbox.register('init_selection', init_selection)
+toolbox.register("individual", tools.initCycle, creator.individual,
+                 (toolbox.init_selection,), n=1)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
 def evaluate_articles(individual, target_size):
     """Evaluates the fitness and return the error on the price and the time
     taken by the order if the chef can cook everything in parallel."""
 
     # go through and don't count the dupes
     indiv_set = set(individual)
+    print(individual[0:10])
     page_links = sum(ALL_FILES[entry][PAGE_LINKS_INDEX] for entry in indiv_set)
     lang_links = sum(ALL_FILES[entry][LANG_LINKS_INDEX] for entry in indiv_set)
     page_views = sum(ALL_FILES[entry][PAGE_VIEWS_INDEX] for entry in indiv_set)
@@ -69,8 +76,10 @@ def cxCounter(ind1, ind2, indpb):
 def mutCounter(individual):
     """Adds or remove an item from an individual"""
     if random.random() > 0.5:
-        the_keys = list(ALL_FILES.keys())
-        
+        the_keys = ALL_FILES_KEYSET
+        indiv_set = set(individual)
+        missing = list(the_keys - indiv_set)
+        individual[random.randint(0,len(individual))] = missing[random.randint(0,len(missing))]
     else:
         individual.remove(individual[random.randint(0,len(individual))])
     return individual,
@@ -92,9 +101,14 @@ def main():
     pop = toolbox.population(n=MU)
     hof = tools.ParetoFront()
 
-    price_stats = tools.Statistics(key=lambda ind: ind.fitness.values[0])
-    time_stats = tools.Statistics(key=lambda ind: ind.fitness.values[1])
-    stats = tools.MultiStatistics(price=price_stats, time=time_stats)
+    size_stats = tools.Statistics(key=lambda ind: ind.fitness.values[0])
+    page_link_stats = tools.Statistics(key=lambda ind: ind.fitness.values[1])
+    lang_link_stats = tools.Statistics(key=lambda ind: ind.fitness.values[2])
+    page_view_stats = tools.Statistics(key=lambda ind: ind.fitness.values[3])
+    stats = tools.MultiStatistics(size=size_stats,
+                                  page_links=page_link_stats,
+                                  lang_links=lang_link_stats,
+                                  page_views=page_view_stats)
     stats.register("avg", numpy.mean, axis=0)
     stats.register("std", numpy.std, axis=0)
     stats.register("min", numpy.min, axis=0)
@@ -107,3 +121,6 @@ def main():
 
 if __name__ == "__main__":
     _, _, hof = main()
+    print("\n And Now, for the hall of fame:")
+    for indiv in hof:
+        print("\nIndividual stats: ", evaluate_articles(indiv, config.target_size))
